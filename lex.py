81db22a7d3518c5db0550a086a81d442354a1105
Eloy Hernandez <eloyhernandezlua@gmail.com>
2021-09-25 16:50:58 -0500

    yacc

--- lex.py
+++ lex.py
@@ -19,3 +19,11 @@ tokens = [
 import ply.lex as lex
 
 #Gramatic rules
+
+import ply.yacc as yacc
+parser = yacc.yacc()
+
+f = open("./pass.txt", "r")
+input = f.read()
+print(input)
+parser.parse(input)
