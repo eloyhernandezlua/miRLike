48b652cff6421bb766e103577fa6562cb7466730
Eloy Hernandez <eloyhernandezlua@gmail.com>
2021-09-25 17:33:45 -0500

    reserved words, tokens and first rules

--- lex.py
+++ lex.py
@@ -3,22 +3,158 @@ import re
 #reserved words
 
 reserved = {
-
+    'Program' : 'PROGRAM',
+    'VARS' : 'VARS',
+    'main' : 'MAIN',
+    'function' : 'FUNCTION',
+    'void' : 'VOID',
+    'int' : 'INT',
+    'float' : 'FLOAT',
+    'char' : 'CHAR',
+    'string' : 'STRING',
+    'bool' : 'BOOL',
+    'return' : 'RETURN',
+    'read' : 'READ',
+    'write' : 'WRITE',
+    'if' : 'IF',
+    'true': 'TRUE',
+    'false': 'FALSE',
+    'then' : 'THEN',
+    'else' : 'ELSE',
+    'while' : 'WHILE',
+    'do' : 'DO',
+    'for' : 'FOR',
+    'to' : 'TO'
 }
 
 #tokens
 
 tokens = [
-
+    'ID',
+    'PTCOMA',
+    'APAR',
+    'CPAR',
+    'ALLA',
+    'CLLA',
+    'COMA',
+    'ACOR',
+    'CCOR',
+    'IGUAL',
+    'DOSPNTS',
+    'STRING',
+    'MAS',
+    'MENOS',
+    'DIV',
+    'POR',
+    'CTEI',
+    'CTEF',
+    'CTEC',
+    'MAYQ',
+    'MENQ',
+    'MAYI',
+    'MENI',
+    'IGUALIGUAL',
+    'DIF'
 ] + list(reserved.values())
 
 
 #types
+t_ignore = "[\t '' \n]"
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+
+t_PTCOMA = r'\;'
+t_APAR = r'\('
+t_CPAR = r'\)'
+t_ALLA = r'\{'
+t_CLLA = r'\}'
+t_COMMA = r'\,'
+t_ACOR = r'\['
+t_CCOR = r'\]'
+t_IGUAL = r'\='
+t_DOSPNTS = r'\:'
+t_STRING = r'\".*\"'
+t_MAS = r'\+'
+t_MENOS = r'\-'
+t_DIV = r'\/'
+t_POR = r'\*'
+t_CTEC = r"\'.\'"
+t_MAYQ = r'\>'
+t_MENQ = r'\<'
+t_MAYI = r'\>\='
+t_MENI = r'\<\='
+t_IGUALIGUAL = r'\=\='
+t_DIF = r'\!\='
+
+def t_CTEF(t):
+    r'\d+\.\d+'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("Float value too large %d", t.value)
+        t.value = 0
+    return t
+
+def t_CTEI(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
 
 #Build lexer
 import ply.lex as lex
 
 #Gramatic rules
+
+def p_empty(t):
+    'empty :'
+    pass
+
+def p_error(t):
+    print("Error sintáctico en '%s'" % t.value)
+
+def p_program(t):
+    'program : PROGRAM ID PTCOMA varss funcs MAIN APAR CPAR ALLA estatutos CLLA'
+    print('Código aceptado')
+
+def p_varss(t):
+    '''varss : VARS vars
+             | empty
+    '''
+
+def p_vars(t):
+    '''vars : tipo DOSPNTS ID varsp vars
+    '''
+
+def p_varsp(t):
+    '''varsp : PTCOMA
+             | COMA varsp
+             | ACOR CTEI CCOR varsp
+    '''
+
+def p_funcs(t):
+    '''funcs : FUNCION funcsp ID APAR params CPAR PTCOMA varss ALLA estatutos CLLA funcs
+             | empty
+    '''
+
+def p_funcsp(t):
+    '''funcsp : VOID
+              | tipo
+    '''
 
 import ply.yacc as yacc
 parser = yacc.yacc()
